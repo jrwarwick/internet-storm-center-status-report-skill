77392770cd89b7fdb39d7de9ab726c93775c81cf
Justin Warwick <justin.warwick@gmail.com>
2022-01-06 21:19:39 +0000

    Actually add everything (that I said I did in the last commit).

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Reports the current global internet storm center condition.
 
 ## About
+The SANS Internet Storm Center (https://isc.sans.edu/about.html) is a free service for general internet community benefit. One of their services is a realtime aggregate global threat level status declaration which may be of interest to IT and security professionals.
 
+TODO: have Mycroft *automatically* poll this status and proactively alert the user on a change in status.
 
 ## Examples
 * "What is the current internet storm center status?"
--- __init__.py
+++ __init__.py
@@ -1,5 +1,27 @@
-from mycroft import MycroftSkill, intent_file_handler
+# MIT LICENSE
+# Mycroft Skill: Internet Storm Center Status Report
+# Copyright (C)2022 Justin Warwick (justin.warwick@gmail.com)
 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""Mycroft skill for ascertaining and reporting the current Internet Storm Center global condition."""
+from mycroft import MycroftSkill, intent_file_handler
+import requests
 
 class InternetStormCenterStatusReport(MycroftSkill):
     def __init__(self):
@@ -7,7 +29,12 @@ class InternetStormCenterStatusReport(MycroftSkill):
 
     @intent_file_handler('report.status.center.storm.internet.intent')
     def handle_report_status_center_storm_internet(self, message):
-        self.speak_dialog('report.status.center.storm.internet')
+        r = requests.get('https://isc.sans.edu/infocon.txt')
+        self.log.info("isc.sans.edu HTTP GET was " + str(r.status_code))
+        self.log.info("isc.sans.edu HTTP GET was " + r.text)
+        self.gui.clear()
+        self.gui.show_text(r.text, "Internet Storm Center condition")
+        self.speak_dialog('report.status.center.storm.internet',{'isc_status': r.text})
 
 
 def create_skill():
